2319d191c832bd41266817ca8830f6abac539188
NicolasTG <nicolastellez@entrenese.com>
2022-05-27 00:04:41 -0500

    [add] cors

--- empresas/src/components/Company/CompanyList.jsx
+++ empresas/src/components/Company/CompanyList.jsx
@@ -1,14 +1,32 @@
-import React from "react";
-
+import React, {useState, useEffect} from "react";
+import * as CompanyServer from './CompanyServer';
 const CompanyList = () => {
+    const [companies,setCompanies] = useState([]);
+    const listCompanies= async() =>{
+        try {
+            const res = await CompanyServer.listCompanies();
+            const data = await res.json();
+            setCompanies(data.companies);
+        } catch (error) {
+            console.log(error);
+            
+        }
+    };
+    useEffect(()=>{
+        listCompanies();
+    },[]);
     return(
         <>
 
-            <h1>CompanyList</h1>
+            <div>
+                {companies.map((company)=>(
+                    <h2>{company.name}</h2>
+                ))}
+            </div>
 
         </>
-    )
-} 
+    );
+}; 
 
 
 export default CompanyList;
